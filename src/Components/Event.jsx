d8d11e79b5ac741e9036f619f34216bd5eb91231
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-03-31 11:29:03 +0530

    event updted

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,4 +1,4 @@
-import { Box, Input, Text, } from '@chakra-ui/react'
+import { Box, Image, Input, Text, } from '@chakra-ui/react'
 import React from 'react'
 import {AiOutlineCalendar, AiOutlineClockCircle} from "react-icons/ai"
 import "./style/Event.css"
@@ -14,7 +14,7 @@ const Event = () => {
          <Box backgroundColor="rgb(214, 240, 247)"  width={{base:"100vw",md:"100vw",lg:"100vw" }} m="auto"  height={"50vh"}>
 
             <Box width="40vw" m="auto" p="4" >
-                <Text fontSize={"1.1rem"} color={"gray.400"} fontWeight="600"> EVENT  <span style={{color:"red"}}>WORKSHOP</span> </Text>
+                <Text fontSize={"1.1rem"} color={"gray.400"} fontWeight="600"> EVENT  <span style={{color:"red"}}> WORKSHOP</span> </Text>
             </Box>
                 <Text fontSize={{base:"1.2rem",md:"1.5rem",lg:"2.4rem" }}  color={"rgb(20, 75, 89)"} fontWeight="600" mt="6">  The  title of this event page </Text>
            
@@ -63,19 +63,140 @@ const Event = () => {
          {/* ------------------------- About Content  --------------------------- */}
 
 
-         <Box border="2px solid red" width="80vw" m="auto" mt="20">
+         <Box  width="60vw" m="auto" mt="40">
                     
-                    <Box border="3px solid black">
-
+                    <Box  width="60vw" m="auto" p="4">
+                     <Text textAlign={"start"} fontSize={"1rem"} letterSpacing="2px" color={"gray.400"} fontWeight="600"> ABOUT THE EVENT </Text>
+                     <Text  textAlign={"start"} fontStyle="italic"  color={"rgb(20, 75, 89)"} fontSize={"1.1rem"} mt="4" fontWeight="600"> What is the event going to be about? What should people expect? </Text>
+                    <Input type="text" color={"rgb(20, 75, 89)"} fontSize={"2rem"}  placeholder='Write here' border="none" mt="4" textDecoration={"none"} />
                     </Box>
-
-
          </Box>
     
+
+       {/* -----------------------  About the speakers --------------------------- */}
+
+       
+       <Box width="60vw" m="auto" mt="20" mb="20" height="60vh">
+                    
+                    <Box  width="60vw" m="auto" p="4">
+                     <Text textAlign={"start"} fontSize={"1rem"} letterSpacing="2px" color={"gray.400"} fontWeight="600"> ABOUT THE SPEAKERS </Text>
+                     <Text  textAlign={"start"} color={"rgb(20, 75, 89)"} fontStyle="italic" fontSize={"1.1rem"} mt="4" fontWeight="600"> Mention a few points about the speakers that are relavent to the topic, to create interest in the event </Text>
+                   </Box>
+                   
+                   {/* ------------------------------- */}
+
+                  <Box border="1px dashed gray" height={"40vh"} p="6"  > 
+                  
+
+                   <Box border="1px solid gray"  width="50vw" height="30vh" m="auto" display={"flex"} justifyContent="space-aroud" gap="5" >
+                    
+                    <Box  width="35vw"  ml="2">
+                       <Box border=".1px solid gray" backgroundColor="rgb(214, 240, 247)"  m="auto" width="150px" height="150px" mt="7" borderRadius={"80"} >
+                       <label>
+                        <Image m="auto" width="50px" mt="9" justifyContent="center" justifyItems={"center"} src="https://icons-for-free.com/iconfiles/png/512/gallery+image+landscape+mobile+museum+open+line+icon-1320183049020185924.png" alt="iamge" />
+                       <Input type="file" display={"none"} />
+                       </label> 
+                       </Box>
+                    </Box>
+
+                      <Box  width="70vw" p="4" mt="4" > 
+                       <Text textAlign={"start"} color={"rgb(20, 75, 89)"} fontSize="1.3rem" mt="4" ml="4" fontWeight={"600"} > Speaker1 </Text>
+                        <Input  placeholder='About The Speaker' outline={"none"} fontWeight={"600"} colorScheme="blue" mt="1" border={"none"} />
+                      </Box>
+
+                   </Box>
+
+                   {/* ------------------ */}
+
+                   <Box p="2" mb="10" >
+                    <Text color="skyblue" fontSize={"1.2rem"} fontWeight={"600"}>
+                   + Add Another 
+                    </Text>
+                    </Box>
+                </Box>
+
+                {/* ---------------- */}
+         </Box>
+    
+        
+        {/* -------------------------- About the modrator --------------------  */}
+
+       
+        <Box width="60vw" m="auto" mt="20" mb="20" height="60vh">
+                    
+                    <Box  width="60vw" m="auto" p="4">
+                     <Text textAlign={"start"} fontSize={"1rem"} letterSpacing="2px" color={"gray.400"} fontWeight="600"> ABOUT THE MODERATOR </Text>
+                     <Text  textAlign={"start"} color={"rgb(20, 75, 89)"} fontStyle="italic" fontSize={"1.1rem"} mt="4" fontWeight="600"> Mention a few points about the moderator </Text>
+                   </Box>
+                   
+                   {/* ------------------------------- */}
+
+                  <Box border="1px dashed gray" height={"40vh"} p="6"  > 
+                  
+
+                   <Box border="1px solid gray"  width="50vw" height="30vh" m="auto" display={"flex"} justifyContent="space-aroud" gap="5" >
+                    
+                    <Box  width="35vw" ml="2" >
+                       <Box border=".1px solid gray" backgroundColor="rgb(214, 240, 247)"  m="auto" width="150px" height="150px" mt="7" borderRadius={"80"} >
+                       <label>
+                        <Image m="auto" width="50px" mt="9" justifyContent="center" justifyItems={"center"} src="https://icons-for-free.com/iconfiles/png/512/gallery+image+landscape+mobile+museum+open+line+icon-1320183049020185924.png" alt="iamge" />
+                       <Input type="file" display={"none"} />
+                       </label> 
+                       </Box>
+                    </Box>
+
+                      <Box  width="70vw" p="4" mt="4" > 
+                       <Text textAlign={"start"} color={"rgb(20, 75, 89)"} fontSize="1.3rem" mt="4" ml="4" fontWeight={"600"} > Moderator1 </Text>
+                        <Input  placeholder='About The Speaker' outline={"none"} fontWeight={"600"} colorScheme="blue" mt="1" border={"none"} />
+                      </Box>
+
+                   </Box>
+
+                   {/* ------------------ */}
+
+                   <Box p="2" mb="10" >
+                    <Text color="skyblue" fontSize={"1.2rem"} fontWeight={"600"}>
+                   + Add Another 
+                    </Text>
+                    </Box>
+                </Box>
+
+                {/* ---------------- */}
+         </Box>
+
+     
+        {/* ----------------------  About reading material ------------------- */}
+
+        <Box width="60vw" m="auto" mt="20" mb="20" height="60vh">
+                    
+                   <Box  width="60vw" m="auto" p="4">
+                     <Text textAlign={"start"} fontSize={"1rem"} letterSpacing="2px" color={"gray.400"} fontWeight="600">READING MATERIAL AND RESOURCES</Text>
+                     <Text  textAlign={"start"} color={"rgb(20, 75, 89)"} fontStyle="italic" fontSize={"1.1rem"}  mt="4" fontWeight="600">Share links or documents that you think people can/should refer to before the event  </Text>
+                     <Input fontSize={"1.2rem"} placeholder='write here'  border={"none"}/>
+                   </Box>
+                   
+                   {/* ------------------------------- */}
+
+                   <Box  width="60vw" m="auto" p="4">
+                     <Text textAlign={"start"} fontSize={"1rem"} letterSpacing="2px" color={"gray.400"} fontWeight="600">JOINING INFO</Text>
+                     <Text  textAlign={"start"} color={"rgb(20, 75, 89)"} fontSize={"1.1rem"} fontStyle="italic" mt="4" fontWeight="600">Share links to the event or live stream  </Text>
+                     <Input fontSize={"1.2rem"} placeholder='write here'  border={"none"}/>
+                   </Box>
+                   
+                   {/* ------------------------------- */}
+
+                 
+
+                {/* ---------------- */}
+         </Box>
+
+
 
     </Box>
     </>
   )
+
 }
 
 export default Event
+
